0832073a211d0e20c7fa9b54ba122e7f430c5463
Alex Harford <harford@gmail.com>
2014-01-31 19:27:07 -0800

    Add script to be run as part of /etc/init.d

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd /mnt/onboard/root && \
+    PYTHONHOME=/mnt/onboard/root/usr \
+    PYTHONPATH=/mnt/onboard/root/usr/lib/python2.7 \
+    LD_LIBRARY_PATH=/mnt/onboard/root/usr/lib/arm-linux-gnueabi:/mnt/onboard/root/lib/arm-linux-gnueabi:/mnt/onboard/root/usr/lib/arm-linux-gnueabi/pulseaudio \
+    REQUESTS_CA_BUNDLE=/mnt/onboard/root/etc/ssl/certs/ca-certificates.crt \
+    usr/bin/python2.7 ../test-pygame.py -l
