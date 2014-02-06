c2debd84daad0d9810230d43c3b30cc84b84b56a
Alex Harford <harford@gmail.com>
2014-02-06 11:24:40 -0800

    Rename test-pygame.py -> kobocal.py

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ cd /mnt/onboard/root && \
     PYTHONPATH=/mnt/onboard/root/usr/lib/python2.7 \
     LD_LIBRARY_PATH=/mnt/onboard/root/usr/lib/arm-linux-gnueabi:/mnt/onboard/root/lib/arm-linux-gnueabi:/mnt/onboard/root/usr/lib/arm-linux-gnueabi/pulseaudio \
     REQUESTS_CA_BUNDLE=/mnt/onboard/root/etc/ssl/certs/ca-certificates.crt \
-    usr/bin/python2.7 ../test-pygame.py -l
+    usr/bin/python2.7 ../kobocal.py -l
